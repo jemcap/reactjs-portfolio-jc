bfa155f8323fdee3e72a2f642fc02f2392768cbd
JCapito <joshuaecapito22@gmail.com>
2023-03-06 19:26:35 +0000

    Set up the routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react'
-import "./index.css"
+import "./index.css";
+import Home from "./components/Home"
+import Project from "./components/Project"
+import ProjectGallery from "./components/ProjectGallery"
+import Contact from "./components/Contact"
+
+import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="project" element={<Project />} />
+        <Route path="projectgallery" element={<ProjectGallery />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
